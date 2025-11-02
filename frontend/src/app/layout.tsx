9a38a6b954256c9449c81eac9ae3d0ec39f1cb0f
Anthony Immenschuh <anthonyimmen279@gmail.com>
2025-11-02 13:23:41 -0600

    deploying analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: "ascii-it",
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
